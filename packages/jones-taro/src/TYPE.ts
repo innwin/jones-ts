177afb6b4b38aa851a16a1ad9298030fb3e820b9
innwin <innwin@live.com>
2022-09-23 08:30:25 +0800

    移除 `jones-taro`

--- packages/jones-taro/src/TYPE.ts
+++
@@ -1,6 +0,0 @@
-export const TYPE = {
-  Logger: Symbol.for("Logger"),
-  Storage: Symbol.for("Storage"),
-  ToastService: Symbol.for("ToastService"),
-  DialogService: Symbol.for("DialogService"),
-};
